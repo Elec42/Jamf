e8ff3ee45d9f5e03d17c14ede2146b7f2608eaed
Elec42 <felix.kroehnert@online.de>
2021-12-19 08:26:56 +0100

    ignore

--- tool/install.sh
+++ tool/install.sh
@@ -6,15 +6,24 @@ then
 	exit
 else
 	echo -e "Compiling JamfEdit..."
-	gcc jamfedit.c -o jamfeditFSG
+	gcc jamfedit.c -o jamfedit
 	if [ $? -eq 0 ]
 	then
+		clear
 		if [ -f "/usr/sbin/jamfedit" ]
 		then
-			echo "Overriding previous install"
-			sudo rm /usr/sbin/jamfeditFSG
+			echo "Overwriting previous installation"
+			sudo rm /usr/sbin/jamfedit
 		fi
 		echo "Linking $(pwd)/jamfedit to /usr/sbin/ ..."
-		sudo ln -s $(pwd)/jamfeditFSG /usr/sbin/jamfeditFSG
+		sudo ln -s $(pwd)/jamfedit /usr/sbin/jamfedit
+		if [ $? -eq 0 ] 
+		then
+			echo -e "\033[1;33mSuccessfully installed JamfEdit\033[m"
+		else
+			echo -e "Linking failed"
+		fi
+	else
+		echo -e "Could not compile!"
 	fi
 fi
